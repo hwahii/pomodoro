41b02830f166259a98eaebbbbe1bc4fe1fb7006f
Huang Yin Chung <in1102@livemail.tw>
2019-07-20 03:27:34 +0800

    Updates

---
+++ precache-manifest.17a2daa8913a29206353f7397e599285.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a005cc6385ca8619c6f598bdb8219025",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "51a935145d2a8cdd451c",
+    "url": "/pomodoro/static/css/main.abec3e8c.chunk.css"
+  },
+  {
+    "revision": "64744946deed31add0b9",
+    "url": "/pomodoro/static/js/2.2c9d3bdf.chunk.js"
+  },
+  {
+    "revision": "51a935145d2a8cdd451c",
+    "url": "/pomodoro/static/js/main.795e8d72.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  },
+  {
+    "revision": "30013910784e4f7bf6e1714268c7d06f",
+    "url": "/pomodoro/static/media/add_red.30013910.svg"
+  },
+  {
+    "revision": "522ba8bee192d5df0a3eaedb5189b2a1",
+    "url": "/pomodoro/static/media/analysis_white.522ba8be.svg"
+  },
+  {
+    "revision": "ad7dbdf696ac2e524657b1a37716b3e1",
+    "url": "/pomodoro/static/media/arrow.ad7dbdf6.svg"
+  },
+  {
+    "revision": "cb415344b27c19dad0a0181def934903",
+    "url": "/pomodoro/static/media/complete.cb415344.svg"
+  },
+  {
+    "revision": "99b596caa2efee46c92373ab4bea4bf1",
+    "url": "/pomodoro/static/media/list_white.99b596ca.svg"
+  },
+  {
+    "revision": "13d1f6b567d153d3153779d7941e82ce",
+    "url": "/pomodoro/static/media/pause_gray.13d1f6b5.svg"
+  },
+  {
+    "revision": "5435d6449a001c96717f14dcb6a49a37",
+    "url": "/pomodoro/static/media/reset_gray.5435d644.svg"
+  },
+  {
+    "revision": "c1791935b805ce1c49248f6d822a98b1",
+    "url": "/pomodoro/static/media/ringtone_white.c1791935.svg"
+  },
+  {
+    "revision": "c7b4294f2ccc77ece704ca28f4f1deb8",
+    "url": "/pomodoro/static/media/start_red.c7b4294f.svg"
+  },
+  {
+    "revision": "cc90d8a54c75186f7a8e1c979a9d9304",
+    "url": "/pomodoro/static/media/tomato_small_color.cc90d8a5.svg"
+  },
+  {
+    "revision": "3ab2cbcca5c444a0c4e4959f4f526e88",
+    "url": "/pomodoro/static/media/tomato_small_gray.3ab2cbcc.svg"
+  }
+]);
